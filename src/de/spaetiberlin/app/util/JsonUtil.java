2f1b61aadbc75d8695491f066c74498c2c6506d0
Johann <git@johann-hofmann.com>
2013-05-26 20:13:51 +0200

    added spaeti info screen

--- src/de/spaetiberlin/app/util/JsonUtil.java
+++ src/de/spaetiberlin/app/util/JsonUtil.java
@@ -13,35 +13,54 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 
 public class JsonUtil {
 
-	public static String getJSON(String url) {
-		StringBuilder builder = new StringBuilder();
-		HttpClient client = new DefaultHttpClient();
-		HttpGet httpGet = new HttpGet(url);
-		try {
-			HttpResponse response = client.execute(httpGet);
-			StatusLine statusLine = response.getStatusLine();
-			int statusCode = statusLine.getStatusCode();
-			if (statusCode == 200) {
-				HttpEntity entity = response.getEntity();
-				InputStream content = entity.getContent();
-				BufferedReader reader = new BufferedReader(new InputStreamReader(content));
-				String line;
-				while ((line = reader.readLine()) != null) {
-					builder.append(line);
+	public static void getJSON(final String url, final Handler handler) {
+
+		Thread thread = new Thread() {
+
+			@Override
+			public void run() {
+
+				StringBuilder builder = new StringBuilder();
+				HttpClient client = new DefaultHttpClient();
+				HttpGet httpGet = new HttpGet(url);
+				try {
+					HttpResponse response = client.execute(httpGet);
+					StatusLine statusLine = response.getStatusLine();
+					int statusCode = statusLine.getStatusCode();
+					if (statusCode == 200) {
+						HttpEntity entity = response.getEntity();
+						InputStream content = entity.getContent();
+						BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+						String line;
+						while ((line = reader.readLine()) != null) {
+							builder.append(line);
+						}
+					} else {
+						Log.e(JsonUtil.class.toString(), "Failed to download file");
+					}
+				} catch (ClientProtocolException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
-			} else {
-				Log.e(JsonUtil.class.toString(), "Failed to download file");
+				Message m = new Message();
+				Bundle b = new Bundle();
+				b.putString("json", builder.toString());
+				m.setData(b);
+				handler.sendMessage(m);
+
 			}
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return builder.toString();
+		};
+
+		thread.start();
+
 	}
 
 }
