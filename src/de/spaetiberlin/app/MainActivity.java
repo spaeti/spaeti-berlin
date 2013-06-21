d180d18a2ec6ee4a8bf27ab30a7562c5de7c68a4
johann <git@johann-hofmann.com>
2013-06-21 06:18:10 +0200

    added missing icons

--- src/de/spaetiberlin/app/MainActivity.java
+++ src/de/spaetiberlin/app/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends SpaetiAbstractActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		
 		final SlidingMenu shopInfo = new SlidingMenu(this);
 		shopInfo.setMode(SlidingMenu.RIGHT);
 		shopInfo.setSlidingEnabled(false);
