92cd0e1a19314db2fba445ce2a8451643da35601
johann <git@johann-hofmann.com>
2013-10-18 17:33:48 +0200

    add simple list in favorites

--- src/de/spaetiberlin/app/FavoriteActivity.java
+++ src/de/spaetiberlin/app/FavoriteActivity.java
@@ -1,5 +1,23 @@
 package de.spaetiberlin.app;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import com.fortysevendeg.android.swipelistview.SwipeListView;
+
 public class FavoriteActivity extends SpaetiAbstractActivity {
 
+  @Override
+  public void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.activity_favorites);
+
+    final SwipeListView swipeListView = (SwipeListView) findViewById(R.id.example_lv_list);
+
+    final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        android.R.layout.simple_list_item_1, new String[] { "as", "asdfasd", "1231" });
+
+    swipeListView.setAdapter(adapter);
+  }
 }
